3fcaf6a7913b13728ce85d21152bc339d779d6ab
wanghm92 <hongmin_wang@ucsb.edu>
2019-06-23 18:42:14 -0700

    2772 games from 17-19

--- scripts/grab_summaries.py
+++ scripts/grab_summaries.py
@@ -1,35 +1,28 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
 import os, sys
 import datetime
-import urllib2
 import codecs
-import subprocess
-
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2
 
 def scrape_rotowire():
     try:
-        os.makedirs("rotowire_raw")
+        os.makedirs("outputs/rotowire_raw")
     except OSError as ex:
         if "File exists" in ex:
-            print ex
+            print(ex)
         else:
             raise ex
 
-    start_day = datetime.date(2014, 1, 1)
-    day = datetime.date(2017, 3, 29)
+    start_day = datetime.date(2017, 3, 30)
+    day = datetime.date(2019, 6, 12)
 
     while day >= start_day:
-        url = "http://www.rotowire.com/basketball/game-recaps.htm?date=%s" % day.strftime("%m/%d/%Y")
+        url = "http://www.rotowire.com/basketball/game-recaps.php?date=%s" % day.strftime("%Y-%m-%d")
         response = urllib2.urlopen(url)
         html = response.read().decode('utf-8', 'ignore')
-        with codecs.open("rotowire_raw/games_%s" % day.strftime("%m_%d_%y"), "w+", "utf-8") as f:
+        with codecs.open("outputs/rotowire_raw/games_%s.html" % day.strftime("%Y-%m-%d"), "w+", "utf-8") as f:
             f.write(html)
         day = day - datetime.timedelta(days=1)
-
-def scrape_sbnation():
-    subprocess.call("scrapy runspider scrape_base.py -o sbnba.json --logfile sbnba.log", shell=True)
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "sbnation":
-        scrape_sbnation()
-    else:
-        scrape_rotowire()
--- scripts/preproc.py
+++ scripts/preproc.py
@@ -1,8 +1,13 @@
 import re
-import json, sys, os, codecs
+import json, sys, os, codecs, pdb, jsonlines
 from nltk import word_tokenize
+import copy
+from tqdm import tqdm
+from pprint import pprint
+from random import shuffle
 
 num_patt = re.compile('([+-]?\d+(?:\.\d+)?)')
+
 
 def to_int(matchobj):
     num = int(round(float(matchobj.group(0)),0)) # rounds to nearest integer
@@ -14,6 +19,7 @@ def to_int(matchobj):
 # - maps everything to an integer; pcts are in [0, 100]
 
 def prep_tokes(thing):
+    out = copy.deepcopy(thing)
     # remove all newline stuff
     summ = thing['summary'].replace(u'\xa0', ' ')
     summ = summ.replace('\\n', ' ').replace('\r', ' ')
@@ -25,19 +31,22 @@ def prep_tokes(thing):
     newtokes = []
     [newtokes.append(toke) if toke[0].isupper() or '-' not in toke
       else newtokes.extend(toke.replace('-', " - ").split()) for toke in tokes]
-    thing['summary'] = newtokes
+    out['summary'] = newtokes
+    return out
+
 
 def prep_nums(thing):
     # do box scores
-    for k, d in thing['box_score'].iteritems():
+    out = copy.deepcopy(thing)
+    for k, d in thing['box_score'].items():
         if "PCT" in k:
-            for idx, pct in d.iteritems():
+            for idx, pct in d.items():
                 if pct is not None:
-                    d[idx] = str(int(round(float(pct)*100, 0)))
+                    out['box_score'][k][idx] = str(int(round(float(pct)*100, 0)))
                 else:
-                    d[idx] = "N/A"
+                    out['box_score'][k][idx] = "N/A"
         elif k == "MIN":
-            for idx, time in d.iteritems():
+            for idx, time in d.items():
                 if time is not None:
                     mins, seconds = None, None
                     try:
@@ -45,20 +54,20 @@ def prep_nums(thing):
                     except AttributeError as ex: # sometimes these are integers i guess
                         mins, seconds = time, 0
                     frac_mins = float(mins) + float(seconds)/60
-                    d[idx] = str(int(round(frac_mins, 0)))
+                    out['box_score'][k][idx] = str(int(round(frac_mins, 0)))
                 else:
-                    d[idx] = "N/A"
+                    out['box_score'][k][idx] = "N/A"
         else: # everything else assumed to be integral
-            for idx, num in d.iteritems():
+            for idx, num in d.items():
                 # see if we can make it a number
                 if num is not None and num != "":
                     try:
                         fnum = float(num)
-                        d[idx] = str(int(round(fnum, 0)))
+                        out['box_score'][k][idx] = str(int(round(fnum, 0)))
                     except ValueError:
                         pass
                 else:
-                    d[idx] = "N/A"
+                    out['box_score'][k][idx] = "N/A"
 
     # do line scores
     linekeys = ['home_line', 'vis_line']
@@ -66,179 +75,122 @@ def prep_nums(thing):
         for k in thing[lk].keys():
             v = thing[lk][k]
             if "PCT" in k and v is not None:
-                thing[lk][k] = str(int(round(float(v)*100, 0)))
+                out[lk][k] = str(int(round(float(v)*100, 0)))
             elif k == "TEAM_WINS_LOSSES" and v is not None:
                 wins, losses = v.split('-')
-                thing[lk]['WINS'] = wins
-                thing[lk]['LOSSES'] = losses
-                del thing[lk][k]
+                out[lk]['WINS'] = wins
+                out[lk]['LOSSES'] = losses
+                del out[lk][k]
             elif v is not None:
                 try:
                     fnum = float(v)
-                    thing[lk][k] = str(int(round(fnum, 0)))
+                    out[lk][k] = str(int(round(fnum, 0)))
                 except ValueError:
                     pass
+
+    return out
 
 
 def add_player_names(thing):
     # will leave off any third name nonsense
-    thing['box_score']['FIRST_NAME'] = {}
-    thing['box_score']['SECOND_NAME'] = {}
-    for k, v in thing['box_score']['PLAYER_NAME'].iteritems():
+    out = copy.deepcopy(thing)
+    out['box_score']['FIRST_NAME'] = {}
+    out['box_score']['SECOND_NAME'] = {}
+    for k, v in thing['box_score']['PLAYER_NAME'].items():
         names = v.split()
-        thing['box_score']['FIRST_NAME'][k] = names[0]
-        thing['box_score']['SECOND_NAME'][k] = names[1] if len(names) > 1 else "N/A"
+        out['box_score']['FIRST_NAME'][k] = names[0]
+        out['box_score']['SECOND_NAME'][k] = names[1] if len(names) > 1 else "N/A"
+    return out
 
 
 def add_team_names(thing):
-    thing['home_line']['CITY'] = thing['home_city']
-    thing['home_line']['NAME'] = thing['home_name']
-    thing['vis_line']['CITY'] = thing['vis_city']
-    thing['vis_line']['NAME'] = thing['vis_name']
+    out = copy.deepcopy(thing)
+    out['home_line']['CITY'] = thing['home_city']
+    out['home_line']['NAME'] = thing['home_name']
+    out['vis_line']['CITY'] = thing['vis_city']
+    out['vis_line']['NAME'] = thing['vis_name']
+    return out
 
+
+def get_or_save_split_keys(all_ids):
+    key_file = "outputs/split_keys.json"
+    if os.path.exists(key_file):
+        print("{} already exists, loading from it".format(key_file))
+        with open(key_file, 'r') as fin:
+            split_keys = json.load(fin)
+    else:
+        shuffle(all_ids)
+        total = len(all_ids)
+        nval, ntest = total*3//20, total*3//20  # 7:1.5:1.5 split
+        ntrain = total - nval - ntest
+        split_keys = {
+            'train_keys': all_ids[:ntrain],
+            'val_keys': all_ids[ntrain:ntrain + nval],
+            'test_keys': all_ids[ntrain + nval:]
+        }
+        with open(key_file, 'w') as fout:
+            json.dump(split_keys, fout)
+
+    print("train :{}, dev: {}, test: {}".format(len(split_keys['train_keys']), len(split_keys['val_keys']), len(split_keys['test_keys'])))
+
+    return split_keys
 
 def prep_roto():
-    with codecs.open("rotoaligned.json", "r", "utf-8") as f:
-        data = json.load(f)
+    data_out = []
+    all_ids = []
+    with jsonlines.open("outputs/aligned.jsonl", "r") as reader:
+        for thing in tqdm(reader.iter(type=dict, skip_invalid=True)):
+            game_id = thing['game_id']
+            all_ids.append(game_id)
 
-    for thing in data:
-        prep_nums(thing)
-        prep_tokes(thing)
-        add_player_names(thing)
-        add_team_names(thing)
-        # rename home and away linescore keys so they don't conflict
-        linekeys = ['home_line', 'vis_line']
-        for lk in linekeys:
-            for k in thing[lk].keys():
-                v = thing[lk][k]
-                thing[lk]["TEAM-" + k] = v
-                del thing[lk][k]
+    split_keys = get_or_save_split_keys(all_ids)
 
-    # read in keys in each (random) split, and split dataset
-    with codecs.open("roto_split_keys.txt", "r", "utf-8") as f:
-        lines = f.readlines()
-        ntrain, nval, ntest = lines[0].split()
-        ntrain, nval, ntest = int(ntrain), int(nval), int(ntest)
-        lines = lines[1:]
-        trkeys = set([thing.strip() for thing in lines[:ntrain]])
-        valkeys = set([thing.strip() for thing in lines[ntrain:ntrain+nval]])
-        testkeys = set([thing.strip() for thing in lines[ntrain+nval:]])
-        train, val, test = [], [], []
-        for thing in data:
-            # get the key
-            key = (thing["day"] + "-" + thing["home_name"] + "-"
-                    + thing["vis_name"] + "-" + "".join(thing["summary"][:5]))
-            if key in testkeys:
-                test.append(thing)
-            elif key in valkeys:
-                val.append(thing)
-            else:
-                train.append(thing)
+    with jsonlines.open("outputs/aligned.jsonl", "r") as reader:
+        for thing in tqdm(reader.iter(type=dict, skip_invalid=True)):
+            thing = prep_nums(thing)
+            thing = prep_tokes(thing)
+            thing = add_player_names(thing)
+            thing = add_team_names(thing)
+            # rename home and away linescore keys so they don't conflict
+            linekeys = ['home_line', 'vis_line']
+            out = copy.deepcopy(thing)
+            for lk in linekeys:
+                for k in thing[lk].keys():
+                    v = thing[lk][k]
+                    out[lk]["TEAM-" + k] = v
+                    del out[lk][k]
+            data_out.append(thing)
+
+    train, val, test = [], [], []
+    for thing in data_out:
+        # get the key
+        key = thing['game_id']
+        if key in split_keys['test_keys']:
+            test.append(thing)
+        elif key in split_keys['val_keys']:
+            val.append(thing)
+        else:
+            train.append(thing)
 
     # finally write everything
     try:
-        os.makedirs("rotowire")
+        os.makedirs("outputs/rotowire_new")
     except OSError as ex:
         if "File exists" in ex:
-            print ex
+            print(ex)
         else:
             raise ex
-    with codecs.open("rotowire/train.json", "w+", "utf-8") as f:
+
+    print("saving train ...")
+    with codecs.open("outputs/rotowire_new/train.json", "w+", "utf-8") as f:
         json.dump(train, f)
-    with codecs.open("rotowire/valid.json", "w+", "utf-8") as f:
+    print("saving valid ...")
+    with codecs.open("outputs/rotowire_new/valid.json", "w+", "utf-8") as f:
         json.dump(val, f)
-    with codecs.open("rotowire/test.json", "w+", "utf-8") as f:
-        json.dump(test, f)
-
-
-def paragraph_stuff(thing, thresh=2):
-    pars = thing["summary"].split("\n\n") # assume more than one newline is a new par
-    newpars = []
-    discarded = 0 # just for threshold stuff
-    for par in pars:
-        if len(par) > 0 and not par.isspace():
-            if "twitter.com" in par or "(@" in par or "(Video" in par:
-                continue
-            if "Final" in par and "1" in par and "2" in par and "3" in par and "4" in par: # stupid FINAL 1 2 3 4 thing
-                break
-            tokes = par.replace('-', ' - ').replace('(', ' ( ').replace(')', ' ) ').split()
-            num_nums = sum((1 for toke in tokes if toke.isdigit() or toke in number_words))
-            if num_nums >= thresh:
-                newpars.append(par)
-            else:
-                discarded += 1
-    thing["summary"] = " *NEWPARAGRAPH* ".join(newpars)
-    return discarded
-
-
-
-def prep_sb(min_summ_len=35):
-    data = []
-    with codecs.open("sbaligned.json", "r", "utf-8") as f:
-        for line in f:
-            obj = json.loads(line.strip())
-            if obj is not None:
-                data.append(obj)
-
-    discarded = 0
-
-    for thing in data:
-        discarded += paragraph_stuff(thing)
-        prep_nums(thing)
-        prep_tokes(thing)
-        add_player_names(thing)
-        add_team_names(thing)
-        # rename home and away linescore keys so they don't conflict
-        linekeys = ['home_line', 'vis_line']
-        for lk in linekeys:
-            for k in thing[lk].keys():
-                v = thing[lk][k]
-                thing[lk]["TEAM-" + k] = v
-                del thing[lk][k]
-
-    print "discarded", discarded
-
-    # get rid of stuff w/ empty or too short summaries
-    data = [thing for thing in data if len(thing["summary"]) >= min_summ_len]
-
-    # read in keys in each (random) split, and split dataset
-    with codecs.open("sb_split_keys.txt", "r", "utf-8") as f:
-        lines = f.readlines()
-        ntrain, nval, ntest = lines[0].split()
-        ntrain, nval, ntest = int(ntrain), int(nval), int(ntest)
-        lines = lines[1:]
-        trkeys = set([thing.strip() for thing in lines[:ntrain]])
-        valkeys = set([thing.strip() for thing in lines[ntrain:ntrain+nval]])
-        testkeys = set([thing.strip() for thing in lines[ntrain+nval:]])
-        train, val, test = [], [], []
-        for thing in data:
-            # get the key
-            key = (thing["day"] + "-" + thing["home_name"] + "-"
-                    + thing["vis_name"] + "-" + "".join(thing["summary"][:10]))
-            if key in testkeys:
-                test.append(thing)
-            elif key in valkeys:
-                val.append(thing)
-            else:
-                train.append(thing)
-
-    # finally write everything
-    try:
-        os.makedirs("sbnation")
-    except OSError as ex:
-        if "File exists" in ex:
-            print ex
-        else:
-            raise ex
-    with codecs.open("sbnation/train.json", "w+", "utf-8") as f:
-        json.dump(train, f)
-    with codecs.open("sbnation/valid.json", "w+", "utf-8") as f:
-        json.dump(val, f)
-    with codecs.open("sbnation/test.json", "w+", "utf-8") as f:
+    print("saving test ...")
+    with codecs.open("outputs/rotowire_new/test.json", "w+", "utf-8") as f:
         json.dump(test, f)
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "sbnation":
-        prep_sb()
-    else:
-        prep_roto()
+    print('prep_roto')
+    prep_roto()
