19181cde8ef9c04917b94689f1cd95c69c64faf7
wanghm92 <hongminwang@Hongmins-MacBook-Pro.local>
2019-08-12 19:00:12 -0700

    graph construction WIP

--- scripts_aaai/process/table2graph.py
+++ scripts_aaai/process/table2graph.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 from collections import Counter, OrderedDict
 from text2num import text2num
 from pprint import pprint
+from itertools import combinations
 DELIM = "￨"
 UNK = 0
 NA = 'N/A'
@@ -10,6 +11,30 @@ PAD_WORD = '<blank>'
 UNK_WORD = '<unk>'
 BOS_WORD = '<s>'
 EOS_WORD = '</s>'
+
+"""
+# team - player: plays_for led_by
+# team <-> team and player <-> player: same rcd_type: >=<
+# Edges:
+1. START vs BENCH:
+    + Teams <-has-> START/BENCH <-has-> Players
+    + START/BENCH <-lead-> Best_Player (or Best_Player PTS)
+    + Teams <-lead-> Best_Player (or Best_Player PTS)
+    + START/BENCH <-has-> SUM_PTS
+    + START SUM_PTS <-compare-> BENCH SUM_PTS for each team
+    + START/BENCH SUM_PTS <-compare-> START/BENCH SUM_PTS across teams
+    !+ (Is this necessary?) START: (p|P)oint guard, (s|S)hooting guard, (s|S)mall forward, (P|p)ower forward, center(/Center)
+
+2. TODOs:
+    + DIFFs not decided
+    + remove N/A players and nodes
+
+# others:
+1. HOME/AWAY
+2. WINNER
+3. STAR/MVP
+
+"""
 
 line_numkeys = [
     'TEAM-PTS',
@@ -57,12 +82,12 @@ print("box_otherkeys: {}".format(len(box_otherkeys)))
 def _get_lookups(records):
     """
     Input:
-        sample: [value|field|rcd_type|ha, ...]
+        sample [list]: [value|field|rcd_type|ha, ...]
     Return:
-        node2idx: {rcd: sample.index(rcd)}
-        entity2nodes: {entity: {rcd_type:(value, rcd)}}
-        player_pairs: {'HOME/AWAY': [(p_i, p_j)]}
-        team_pairs: {'HOME/AWAY': [(t_i, t_j)]}
+        node2idx     [dict]: {rcd: sample.index(rcd)}
+        entity2nodes [dict]: {entity: {rcd_type:(value, rcd)}}
+        player_pairs [dict]: {'HOME/AWAY': [(p_i, p_j)]}
+        team_pairs   [dict]: {'HOME/AWAY': [(t_i, t_j)]}
     """
     node2idx = {}
     entity2nodes = {}
@@ -71,7 +96,7 @@ def _get_lookups(records):
     for idx, rcd in enumerate(records):
         node2idx[rcd] = idx
         value, field, rcd_type, ha = rcd.split(DELIM)
-        entity2nodes.setdefault(field, {rcd_type:(value, rcd)})
+        entity2nodes.setdefault(field, {rcd_type: (value, rcd)})
         entity2nodes[field][rcd_type] = (value, rcd)
         if rcd_type == 'TEAM-NAME':
             ha2team[ha] = rcd
@@ -81,9 +106,7 @@ def _get_lookups(records):
     player_pairs = {'HOME': [], 'AWAY':[]}
     for k, player in ha2player.items():
         player = [rcd.split(DELIM)[0] for rcd in player]
-        for idx, left in enumerate(player[:-1]):
-            for right in player[idx+1:]:
-                player_pairs[k].append((left, right))
+        player_pairs[k].extend(combinations(player, 2))
     return node2idx, entity2nodes, player_pairs, ha2player, ha2team
 
 def _get_pairwise(left, right, rcd_types, entity2nodes, node2idx):
@@ -131,44 +154,16 @@ def _get_team_player(ha2player, ha2team, node2idx):
             edges[(player_id, team_id)] = label
     return edges
 
-"""
-# team - player: plays_for led_by
-# team <-> team and player <-> player: same rcd_type: >=<
-# Edges:
-1. START vs BENCH:
-    + Teams <-has-> START/BENCH <-has-> Players
-    + START/BENCH <-lead-> Best_Player (or Best_Player PTS)
-    + Teams <-lead-> Best_Player (or Best_Player PTS)
-    + START/BENCH <-has-> SUM_PTS
-    + START SUM_PTS <-compare-> BENCH SUM_PTS for each team
-    + START/BENCH SUM_PTS <-compare-> START/BENCH SUM_PTS across teams
-    + (Is this necessary?) START: (p|P)oint guard, (s|S)hooting guard, (s|S)mall forward, (P|p)ower forward, center(/Center) 
-
-2. TODOs:
-    + DIFFs not decided
-    + remove N/A players and nodes
-
-# others:
-1. HOME/AWAY
-2. WINNER
-3. STAR/MVP
-
-"""
-
-DATA = 'train'
+DATA = 'test'
 BASE="/mnt/cephfs2/nlp/hongmin.wang/table2text/boxscore-data"
-fname = "{}/scripts_aaai/new_dataset/new_extend/{}/src_{}.norm.trim.txt".format(BASE, DATA, DATA)
-fout = "{}/scripts_aaai/new_dataset/new_extend/{}/edge_combo_{}.jsonl".format(BASE, DATA, DATA)
-# fedge_left = "{}/scripts_aaai/new_dataset/new_extend/{}/edge_index_left_{}.txt".format(DATA, DATA)
-# fedge_right = "{}/scripts_aaai/new_dataset/new_extend/{}/edge_index_right_{}.txt".format(DATA, DATA)
-# flabel = "{}/scripts_aaai/new_dataset/new_extend/{}/edge_label_{}.txt".format(DATA, DATA)
-cnt1 = []
-cnt2 = []
+fname = "{}/scripts_aaai/new_dataset/new_ncpcc/{}/src_{}.norm.trim.ncp.full.txt".format(BASE, DATA, DATA)
+fout = "{}/scripts_aaai/new_dataset/new_ncpcc/{}/edges_{}.ncp.jsonl".format(BASE, DATA, DATA)
+
 with io.open(fname, 'r', encoding='utf-8') as fin, jsonlines.open(fout, 'w') as writer:
 
     inputs = fin.read().strip().split('\n')
     for sample in tqdm(inputs):
-        combo = {}
+        temp = {}
         records = sample.strip().split()
         node2idx, entity2nodes, player_pairs, ha2player, ha2team = _get_lookups(records)
         home = ha2team['HOME'].split(DELIM)[0]
@@ -176,34 +171,12 @@ with io.open(fname, 'r', encoding='utf-8') as fin, jsonlines.open(fout, 'w') as 
         team_pair = [(home, away)]
         for pair_list, key_list in zip([player_pairs['HOME'], player_pairs['AWAY'], team_pair], [box_numkeys, box_numkeys, line_numkeys]):
             for left, right in pair_list:
-                combo.update(_get_pairwise(left, right, key_list, entity2nodes, node2idx))
+                temp.update(_get_pairwise(left, right, key_list, entity2nodes, node2idx))
 
-        # cnt1.append(len(node2idx))
         tmp = _get_team_player(ha2player, ha2team, node2idx)
-        # cnt2.append(len(tmp))
-        combo.update(tmp)
-        left_edges = []
-        right_edges = []
-        label = []
-        temp = {}
-        for (left, right), lab in combo.items():
-            left_edges.append(str(left))
-            right_edges.append(str(right))
+        temp.update(tmp)
+        combo = {}
+        for (left, right), lab in temp.items():
             key = '{},{}'.format(left, right)
-            temp[key] = lab
-            label.append(lab)
-        writer.write(temp)
-    # print(Counter(cnt1))
-    # print(Counter(cnt2))
-
-    '''
-    fout_edge_left.write('{}\n'.format(' '.join(left_edges)))
-    fout_edge_right.write('{}\n'.format(' '.join(right_edges)))
-    fout_label.write('{}\n'.format(' '.join(label)))
-    print(len(records))
-    print(len(edges))
-    print(edges)
-#         for (i, j), t in edges.items():
-#             print("{} ** {} ** {}".format(DELIM.join(records[i].split(DELIM)[:3]), t, DELIM.join(records[j].split(DELIM)[:3])))
-    sys.exit(0)
-    '''
+            combo[key] = lab
+        writer.write(combo)
